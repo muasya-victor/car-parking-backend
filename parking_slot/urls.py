c42191e86a0b171e0d52baacb2905c386484ff36
muasya-victor <vicmwe184@gmail.com>
2024-11-29 16:48:00 +0300

    slot report

--- parking_slot/urls.py
+++ parking_slot/urls.py
@@ -1,7 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import ParkingSlotViewSet
+from .views import ParkingSlotViewSet,PDFView
+from django.urls import path, include
+
 
 router = DefaultRouter()
 router.register(r'parking-slots', ParkingSlotViewSet, basename='parking_slots')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('slot/report/', PDFView.as_view(), name='slot-report-pdf')
+]
--- parking_slot/views.py
+++ parking_slot/views.py
@@ -1,8 +1,17 @@
+from io import BytesIO
+
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from xhtml2pdf import pisa
 
 from parking_slot.models import ParkingSlot
 from parking_slot.serializers import ParkingSlotSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import viewsets, permissions, status
+
+from users.models import CustomUser
 
 
 class ParkingSlotViewSet(viewsets.ModelViewSet):
@@ -15,3 +24,27 @@ class ParkingSlotViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         # Filter the queryset to return only available parking slots
         return ParkingSlot.objects.filter(parking_slot_available=True)
+
+
+class PDFView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        slots = ParkingSlot.objects.all()
+
+        serializer = ParkingSlotSerializer(
+            slots, many=True, context={'request': request})
+        context = {'slots': serializer.data}
+
+        html_string = render_to_string('slot_report.html', context)
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=slot_report.pdf'
+
+        result = BytesIO()
+        pdf = pisa.pisaDocument(BytesIO(html_string.encode("UTF-8")), result)
+        if not pdf.err:
+            response.write(result.getvalue())
+            return response
+        return HttpResponse('We had some errors <pre>' + html_string +'</pre>')
