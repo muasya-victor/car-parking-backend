01d2f5f1315afceb2bb1285cba7e66a76adba138
muasya-victor <vicmwe184@gmail.com>
2024-12-10 15:37:59 +0300

    create booking report

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookingViewSet, BookingHistoryViewSet
+from .views import BookingViewSet, BookingHistoryViewSet, PDFView
+from django.urls import path, include
+
 
 router = DefaultRouter()
 router.register(r'bookings', BookingViewSet)
 router.register(r'booking-history', BookingHistoryViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('booking/report/', PDFView.as_view(), name='slot-report-pdf')
+]
--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from .models import Booking, BookingHistory
 from .serializers import BookingSerializer, BookingHistorySerializer
+from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from xhtml2pdf import pisa
+from io import BytesIO
 
 
 class BookingViewSet(viewsets.ModelViewSet):
@@ -30,3 +36,44 @@ class BookingHistoryViewSet(viewsets.ModelViewSet):
     queryset = BookingHistory.objects.all()
     serializer_class = BookingHistorySerializer
     permission_classes = [AllowAny]  # Allow access without login
+
+
+from django.utils.timezone import now
+from django.db.models import Prefetch
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from xhtml2pdf import pisa
+from io import BytesIO
+from parking_slot.models import ParkingSlot
+from booking.models import Booking
+
+
+class PDFView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # Fetch parking slots with related bookings
+        slots = ParkingSlot.objects.prefetch_related(
+            Prefetch(
+                'booking_set',
+                queryset=Booking.objects.all()
+            )
+        )
+
+        context = {
+            'current_date': now().strftime('%Y-%m-%d %H:%M:%S'),
+            'slots': slots
+        }
+
+        # Render the template
+        html_string = render_to_string('slot_report.html', context)
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename=slot_report.pdf'
+
+        # Generate PDF
+        result = BytesIO()
+        pdf = pisa.pisaDocument(BytesIO(html_string.encode("UTF-8")), result)
+        if not pdf.err:
+            response.write(result.getvalue())
+            return response
+        return HttpResponse('We had some errors <pre>' + html_string + '</pre>')
